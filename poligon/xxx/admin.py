bd56d93b1247e83bd3bf036d6ac0f8cc05425d57
vladiuse <vladiuse@gmail.com>
2023-07-11 19:57:16 +0300

    add images in admin

--- poligon/xxx/admin.py
+++ poligon/xxx/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Domain, SiteImages, ImageEx
+from django.utils.html import mark_safe
+
+
+def img_tag(src):
+    return mark_safe(f'<img src="{src}" width="50px" height="50px" />')
 
 
 def load_image(modeladmin, request, queryset):
@@ -8,8 +13,7 @@ def load_image(modeladmin, request, queryset):
 
 
 class SiteImagesAdmin(admin.ModelAdmin):
-    list_display = ['id','domain','image_url','orig_img','thumb',]
-
+    list_display = ['id', 'domain', 'image_url', 'orig_img', 'orig_tag', 'thumb', 'thumb_tag']
     actions = [load_image, 'make_thumb', 'delete_images']
 
     @admin.action(description='Make Thumb')
@@ -21,6 +25,14 @@ class SiteImagesAdmin(admin.ModelAdmin):
     def delete_images(self, request, queryset):
         for m in queryset:
             m.delete_images()
+
+    def orig_tag(self, obj):
+        if obj.orig_img:
+            return img_tag(obj.orig_img.url)
+
+    def thumb_tag(self, obj):
+        if obj.thumb:
+            return img_tag(obj.thumb.url)
 
 
 admin.site.register(Domain)
